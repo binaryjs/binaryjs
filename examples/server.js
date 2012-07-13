0278183f370da61cd0e4000baf01f082bf42a8d1
ericz <really.ez@gmail.com>
2012-07-13 10:58:20 -0700

    somewhat working + examples

--- examples/server.js
+++ examples/server.js
@@ -1,15 +1,24 @@
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
+var express = require('express');
+var app = express.createServer();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(9001);
 
 var server = BinaryServer({port: 9000});
 
-server.on('connection', function(user){
+server.on('connection', function(client){
   
   console.log('New user connected');
   
-  var file = fs.createReadStream('flower.png');
-
+  var stream = client.createStream();
+  var file = fs.createReadStream(__dirname + '/big.avi');
   file.pipe(stream);
+  stream.on('drain', function(){console.log('drain')});
+  stream.on('close', function(){console.log('close')});
   
 });
 
