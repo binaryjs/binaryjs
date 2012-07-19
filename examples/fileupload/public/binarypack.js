9c7627ddaa18c96e07ab82444d8a650501066d8e
ericz <really.ez@gmail.com>
2012-07-18 21:40:18 -0700

    updated example deps

--- examples/fileupload/public/binarypack.js
+++ examples/fileupload/public/binarypack.js
@@ -1,4 +1,4 @@
-/*! binarypack.js build:0.0.1, development. Copyright(c) 2012 Eric Zhang <eric@ericzhang.com> MIT Licensed */
+/*! binarypack.js build:0.0.2, development. Copyright(c) 2012 Eric Zhang <eric@ericzhang.com> MIT Licensed */
 (function(exports){
 exports.binaryFeatures = {
   useBlobBuilder: (function(){
@@ -383,7 +383,7 @@ Packer.prototype.pack = function(value){
 
 Packer.prototype.pack_bin = function(blob){
   var length = blob.length || blob.byteLength || blob.size;
-  if (length <= 0x1f){
+  if (length <= 0x0f){
     this.pack_uint8(0xa0 + length);
   } else if (length <= 0xffff){
     this.bufferBuilder.append(0xda) ;
@@ -400,7 +400,7 @@ Packer.prototype.pack_bin = function(blob){
 
 Packer.prototype.pack_string = function(str){
   var length = str.length;
-  if (length <= 0x1f){
+  if (length <= 0x0f){
     this.pack_uint8(0xb0 + length);
   } else if (length <= 0xffff){
     this.bufferBuilder.append(0xd8) ;
