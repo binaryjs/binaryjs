dbda4a44d86a18bd7cf329b1d250c0e9918fbff7
ericz <really.ez@gmail.com>
2012-07-25 11:30:53 -0700

    hw example

--- examples/helloworld/server.js
+++ examples/helloworld/server.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 var server = BinaryServer({port: 9000});
 // Wait for new user connections
 server.on('connection', function(client){
-  console.log('New user connected', client.id);
   // Stream a flower as a hello!
   var file = fs.createReadStream(__dirname + '/flower.png');
   client.send(file); 
