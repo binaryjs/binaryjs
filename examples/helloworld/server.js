8047faa4b25c335cc57a137d0bac0dec25703559
ericz <really.ez@gmail.com>
2012-07-25 11:10:05 -0700

    freshen up hw example

--- examples/helloworld/server.js
+++ examples/helloworld/server.js
@@ -1,37 +1,13 @@
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
-var express = require('express');
-var app = express.createServer();
-
-app.use(express.static(__dirname + '/public'));
-app.listen(9001);
-
-
-
-
-
 
 // Start Binary.js server
 var server = BinaryServer({port: 9000});
-
 // Wait for new user connections
 server.on('connection', function(client){
-  
-  console.log('New user connected');
-  
-  // Incoming stream from browsers
-  client.on('stream', function(stream){
-    var w = fs.createWriteStream(__dirname+ '/' + stream.id+'.png');
-    stream.pipe(w);
-  });
-  
+  console.log('New user connected', client.id);
   // Stream a flower as a hello!
-  var file = fs.readFileSync(__dirname + '/flower.png');
-  client.send(file);
-  
-  setInteval(function(){
-    console.log(server.clients, client.streams);
-  }, 1000);
-  
+  var file = fs.createReadStream(__dirname + '/flower.png');
+  client.send(file); 
 });
 
